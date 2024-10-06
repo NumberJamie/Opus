ff02ef945efccc20d5699d5f1437a330db437680
NumberJamie <jamievanessen@gmail.com>
2024-10-06 23:55:44 +0200

    HTTP status

---
+++ source/HTTPStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace source;
+
+final class HTTPStatus {
+    // 4xx client errors
+    const int FORBIDDEN = 403;
+    const int NOT_FOUND = 404;
+    const int METHOD_NOT_ALLOWED = 405;
+}
--- source/Router.php
+++ source/Router.php
@@ -6,11 +6,11 @@ class Router {
     private array $routes = [];
     private array $availableMethods = [];
 
-    private function append_route(string $method, string $path, string $controller){
+    private function append_route(string $method, string $path, string $controller): void {
         if (!in_array($method, $this->availableMethods)){
             $this->availableMethods[] = $method;
         }
-        $this->routes[] = ['path' => $path, "controller" => $controller, "method" => $method];
+        $this->routes[] = compact('path', 'controller', 'method');
     }
 
     function get(string $path, string $controller): void {
@@ -22,8 +22,9 @@ class Router {
     }
 
     function respond(string $path, string $method): void {
+        // TODO: clean the path string
         if (!in_array($method, $this->availableMethods)){
-            $this->abort(405);
+            $this->abort(HTTPStatus::METHOD_NOT_ALLOWED);
         }
         foreach ($this->routes as $route){
             if ($route['path'] != $path or $route['method'] != $method) continue;
@@ -33,9 +34,9 @@ class Router {
         $this->abort();
     }
 
-    function abort(int $statusCode = 404): void {
+    function abort(int $statusCode = HTTPStatus::NOT_FOUND): void {
         http_response_code($statusCode);
-        // TODO: return error php file
+        // TODO: require error php file
         die();
     }
 }
